38d9ca7bd424d9840471b05d95948a8ca742aa15
roshansafika <132439942+roshansafika@users.noreply.github.com>
2023-05-03 23:22:22 -0700

    Add files via upload

---
+++ Flask/app.py
@@ -0,0 +1,22 @@
+from flask import flask,render_template,request
+import numpy as np
+import pickle
+model=pickle.load(open(r"model1.pkl",'rb'))
+@app.route("/home")
+def home():
+return render_template('home.html')
+@app.route("/predict")
+def home1();
+return render_template('predict.html')
+@app.route("/pred",methods=['POST','GET'])
+def predict();
+x=[[int(x)for x in request.from.values()]]
+print(x)
+x=np.array(x)
+print(x.shape)
+print(x)
+pred=model.predict(x)
+print(pred)
+return render_template('submit.html',prediction_text=pred
+if __name__"__main__":
+app.run(debug=false)
